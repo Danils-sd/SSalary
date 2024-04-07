0b0c7b69e41cfb197abe00d381c2db85e3491ace
Danils-sd <hlebushek.sd@gmail.com>
2024-04-07 21:02:53 +0300

    folders settings

---
+++ front/src/components/Router.js
@@ -0,0 +1,12 @@
+import "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router(){
+    return <BrowserRouter>
+    <Routes>
+
+    </Routes>
+    </BrowserRouter>
+}
+
+export default Router;
